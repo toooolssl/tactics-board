a446f296765cbca496a85cbda8a9e90a304b233f
Kevin Marín Zamarrón <kmarin@toools.es>
2025-03-28 13:12:20 +0100

    imagenes logos

--- src/AppConfigs.js
+++ src/AppConfigs.js
@@ -1,26 +1,26 @@
 const AppConfigs = {
-	default: {
-		useFirebase: true,
-		siteHomeURL: null,
-		siteLogoURL: null,
-		pitchCenterADURL: null,
-		thumbnailWidth: 320,	// height calculated from proportions
-	},
-	coatchingfutsal: {
-		useFirebase: false,
-		siteHomeURL: '/',
-		siteLogoURL: '/Img/logo-toools.png',
-		pitchCenterADURL: '/TacticsBoard/CenterAdvert',
-		signInURL: '/Account/LoginRegister',
-		signOutURL: '/Account/LogOff',
-		getUserURL: '/Account/GetUser',
-		saveURL: '/TacticsBoard/SaveTactic',
-		loadURL: '/TacticsBoard/TacticsLoad',
-		loadSharedURL: '/TacticsBoard/TacticsLoadShared',
-		listURL: '/TacticsBoard/GetTacticsList',
-		deleteURL: '/TacticsBoard/DeleteTactic',
-		thumbnailWidth: 1280,	// height calculated from proportions
-	}
+    default: {
+        useFirebase: true,
+        siteHomeURL: null,
+        siteLogoURL: null,
+        pitchCenterADURL: null,
+        thumbnailWidth: 320,    // height calculated from proportions
+    },
+    coatchingfutsal: {
+        useFirebase: false,
+        siteHomeURL: '/',
+        siteLogoURL: '/img/logo-toools.png',       // Nueva ruta relativa a public/
+        pitchCenterADURL: '/img/isquad-blanco-completo.png', // Nueva ruta relativa a public/
+        signInURL: '/Account/LoginRegister',
+        signOutURL: '/Account/LogOff',
+        getUserURL: '/Account/GetUser',
+        saveURL: '/TacticsBoard/SaveTactic',
+        loadURL: '/TacticsBoard/TacticsLoad',
+        loadSharedURL: '/TacticsBoard/TacticsLoadShared',
+        listURL: '/TacticsBoard/GetTacticsList',
+        deleteURL: '/TacticsBoard/DeleteTactic',
+        thumbnailWidth: 1280,   // height calculated from proportions
+    }
 }
 
 export default AppConfigs;
